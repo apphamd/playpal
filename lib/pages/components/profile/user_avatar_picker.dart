e72f43e2df3da27c2d069fa9caf4d56ee111265a
Albert Pham <apphamd@gmail.com>
2023-11-05 17:26:00 -0600

    Added images to dog card

--- lib/pages/components/profile/user_avatar_picker.dart
+++ lib/pages/components/profile/user_avatar_picker.dart
@@ -40,53 +40,54 @@ class _UserAvatarPickerState extends State<UserAvatarPicker> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: users.snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('An error has occured ${snapshot.error}'),
-            );
-          }
-          var currentUser;
-          String profilePic = '';
-          if (snapshot.hasData) {
-            for (var user in snapshot.data!.docs) {
-              if (widget.currentUser.userId == user.id) {
-                currentUser = user.data() as Map;
-              }
-            }
-            currentUser['profile_pic'] == null
-                ? ()
-                : profilePic = currentUser['profile_pic'];
-          }
-
-          return Stack(
-            children: <Widget>[
-              profilePic.isNotEmpty
-                  ? CircleAvatar(
-                      radius: 72,
-                      backgroundImage: NetworkImage(profilePic),
-                      backgroundColor: Colors.white,
-                    )
-                  : const CircleAvatar(
-                      radius: 72,
-                      backgroundColor: Colors.grey,
-                      // backgroundImage: NetworkImage(
-                      //     "https://cdn0.iconfinder.com/data/icons/google-material-design-3-0/48/ic_account_circle_48px-1024.png"),
-                    ),
-              Positioned(
-                bottom: 0,
-                right: 7,
-                child: IconButton(
-                  onPressed: selectImage,
-                  icon: Icon(
-                    Icons.add_a_photo,
-                    color: Colors.amber[600],
-                  ),
-                ),
-              )
-            ],
+      stream: users.snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('An error has occured ${snapshot.error}'),
           );
-        });
+        }
+        var currentUser;
+        String profilePic = '';
+        if (snapshot.hasData) {
+          for (var user in snapshot.data!.docs) {
+            if (widget.currentUser.userId == user.id) {
+              currentUser = user.data() as Map;
+            }
+          }
+          currentUser['profile_pic'] == null
+              ? ()
+              : profilePic = currentUser['profile_pic'];
+        }
+
+        return Stack(
+          children: <Widget>[
+            profilePic.isNotEmpty
+                ? CircleAvatar(
+                    radius: 72,
+                    backgroundImage: NetworkImage(profilePic),
+                    backgroundColor: Colors.white,
+                  )
+                : const CircleAvatar(
+                    radius: 72,
+                    backgroundColor: Colors.grey,
+                    // backgroundImage: NetworkImage(
+                    //     "https://cdn0.iconfinder.com/data/icons/google-material-design-3-0/48/ic_account_circle_48px-1024.png"),
+                  ),
+            Positioned(
+              bottom: 0,
+              right: 7,
+              child: IconButton(
+                onPressed: selectImage,
+                icon: Icon(
+                  Icons.add_a_photo,
+                  color: Colors.amber[600],
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
   }
 }
